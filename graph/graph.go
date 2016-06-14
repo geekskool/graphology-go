516656c83b66a077930165454c57fb664f531ad6
jkurambhatti <jkurambhatti2992@gmail.com>
2016-06-14 11:03:10 +0530

    graph building functions

--- graph/graph.go
+++ graph/graph.go
@@ -1,36 +1,4 @@
-package graphology
-
-type anything interface{}
-
-type Vertex struct {
-	Id    string              `json:"id"`
-	Label []string            `json:"label"`
-	In    []string            `json:"-"`
-	Out   []string            `json:"-"`
-	Props map[string]anything `json:"props,omitempty"`
-}
-
-type Edge struct {
-	Id    string              `json:"id"`
-	Label string              `json:"label"`
-	Tail  string              `json:"tail,omitempty"`
-	Head  string              `json:"head,omitempty"`
-	Props map[string]anything `json:"props,omitempty"`
-}
-
-type Graph struct {
-	Vertices     []*Vertex          `json:"vertices,omitempty"`
-	Edges        []*Edge            `json:"edges,omitempty"`
-	VertexIndex  map[string]*Vertex `json:"-"`
-	EdgeIndex    map[string]*Edge   `json:"-"`
-	AutoVertexId int                `json:"-"`
-	AutoEdgeId   int                `json:"-"`
-}
-
-type Query struct {
-	graph   Graph
-	results []Vertex
-}
+package graph
 
 func CreateGraph() Graph {
 	var graph Graph
